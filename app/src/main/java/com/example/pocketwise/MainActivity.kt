0eb8d709bc75388eb645a05f008a24219552b55b
Gaurav Jain <gauravjain0781@gmail.com>
2024-09-07 18:09:45 +0530

    Splash Screen - Gaurav

--- app/src/main/java/com/example/pocketwise/MainActivity.kt
+++ app/src/main/java/com/example/pocketwise/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.example.pocketwise
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var topAnimation : Animation
+    private lateinit var bottomAnimation : Animation
+    private lateinit var logo : ImageView
+    private lateinit var app : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash)
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        logo = findViewById(R.id.logo_image)
+        app = findViewById(R.id.app_name)
+
+        logo.startAnimation(topAnimation)
+        app.startAnimation(bottomAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
