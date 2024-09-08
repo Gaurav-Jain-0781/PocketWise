de13af96f76e71a47937c4b88fd4d276dc0a864e
Gaurav Jain <gauravjain0781@gmail.com>
2024-09-08 23:28:20 +0530

    Login Activity - Gaurav

--- app/src/main/java/com/example/pocketwise/LoginActivity.kt
+++ app/src/main/java/com/example/pocketwise/LoginActivity.kt
@@ -1,14 +1,54 @@
 package com.example.pocketwise
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var username : EditText
+    private lateinit var password : EditText
+    private lateinit var loginButton : Button
+    private lateinit var signUpTextView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.splash)
+        setContentView(R.layout.login)
 
+        loginButton = findViewById(R.id.login_button)
+        signUpTextView = findViewById(R.id.signupLink)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+
+        signUpTextView.setOnClickListener(){
+            startActivity(Intent(this, SignUpActivity:: class.java))
+        }
+
+        loginButton.setOnClickListener(){
+            login()
+        }
+    }
+
+    private fun login(){
+        val name = username.text.toString()
+        val pass = password.text.toString()
+
+        if(name.isEmpty() || pass.isEmpty()){
+            Toast.makeText(this,"Fill all Fields!", Toast.LENGTH_LONG).show()
+            return
+        } else {
+            fetchData(name, pass)
+        }
+    }
+
+    private fun fetchData(name: String, password: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
